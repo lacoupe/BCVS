b7547ace6b377f2f92d212b2bd28fa8e082e2c68
lacoupe <leopold.coupet@epfl.ch>
2021-11-22 12:00:21 +0100

    update

--- SW/DL/helpers.py
+++ SW/DL/helpers.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 import calendar
+import os
 
 
 def last_month(date):
@@ -127,7 +128,7 @@ def annual_alpha_plot(perf_bench, df_pred, daily_returns):
     plt.ylabel('Alpha (%)')
     plt.xlabel('Year')
     plt.title('Annual Return of the model (gross) over Benchmark Return : ')
-    plt.savefig('plots/excess_return.png', dpi=200)
+    plot_path = os.path.join(os.path.dirname(__file__)) + 'plots/excess_return.png'
     plt.show()
     
     
